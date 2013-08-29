cbb732e019bad2c90cc60842cb12e7e49729f282
Sean Perkins <perkinsean@gmail.com>
2013-08-29 13:18:53 -0400

    cleaned up some commented out stuff, added comment

--- bubbleSort.py
+++ bubbleSort.py
@@ -1,23 +1,20 @@
 
 datData = [1,3,25,18,54,2,77,16,18,19,20,32,10,43,33,46,100,165,1024,566,343,23,45]
 
-
-#isSorted = False
 def bubbleSort(datData):
 	global isSorted
 	isSorted = False
 	for i in range(len(datData)-1):
 		if datData[i] >  datData[i+1]:
+			#swap!
 			datData[i], datData[i+1] = datData[i+1], datData[i]
 			i=0
 			print datData
-			#return isSorted == False
 			
 		else: 
 			isSorted = True
 	return True
 
 def main():
-	#while isSorted == False:
 	isSorted = bubbleSort(datData)
 main()
