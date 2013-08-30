fe86ff67ff0bae16e5b2736eabb1261c5eef598c
Sean Perkins <perkinsean@gmail.com>
2013-08-29 20:42:14 -0400

    not much, pushing for Ethan

--- bubbleSort.py
+++ bubbleSort.py
@@ -1,39 +1,38 @@
 import random
+import string
 
 #datData = [1,3,25,18,54,2,77,16,18,19,20,32,10,43,33,46,100,165,1024,566,343,23,45]
 
 def generateNums():
-	global f
-	f = open("numbers.txt", 'w')
-	nums = []
-	for i in random.sample(range(500),500):
-		nums.append(i)
-	f.write('%s' % nums)
 
+	f = open("numbers.txt", 'w')
+	#nums = []
+	for i in random.sample(range(500),500):
+		print i
+		f.write('%s,' % i)
 	#print nums
-	return f
+	#return f
 	f.close()
 
-def bubbleSort(f2):
+def bubbleSort():
 	global isSorted
 	isSorted = True
 	while isSorted:
 		isSorted = False
-		with open("numbers.txt") as f2:
-			lines = f2.readlines()
-			print lines
-		
-		'''
-		for i in range(len(datData)-1):
-			if datData[i] >  datData[i+1]:
-				#swap!
-				datData[i], datData[i+1] = datData[i+1], datData[i]
-				#i=0
-				print datData
-				isSorted = True
+		with open("numbers.txt", 'r') as f:
+			li = map(int, string.split(f.readline()))
+			print li
+			'''
+			for i in range(len(lines)-1):
+				if lines[i] >  lines[i+1]:
+					#swap!
+					lines[i], lines[i+1] = lines[i+1], lines[i]
+					#i=0
+					print lines
+					isSorted = True
 	return None
-	'''
+'''
 def main():
 	generateNums()
-	isSorted = bubbleSort(f)
+	isSorted = bubbleSort()
 main()
